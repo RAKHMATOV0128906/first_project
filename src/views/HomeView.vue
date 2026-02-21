3a25271e1d7062217a83be2535a83d698675a058
hayotbekdeveloper <hayotrakhmatov1@gmail.com>
2026-02-21 18:37:19 +0500

    card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,20 @@
 <template>
   <NavbarComponentsView></NavbarComponentsView>
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-3" v-for="(item,index) in 8" :key="index">
+        <ImgCardComponentsView></ImgCardComponentsView>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import NavbarComponentsView from "@/components/NavbarComponentsView.vue";
-
+import ImgCardComponentsView from "@/components/ImgCardComponentsView.vue";
 export default {
   components: {
-    NavbarComponentsView,
+    NavbarComponentsView, ImgCardComponentsView
   }
 }
 </script>
